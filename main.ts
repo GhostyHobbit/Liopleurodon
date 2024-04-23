0ca856d36e051c61599dc5c2941537e81b0a6c97
Lucas <145674794+GhostyHobbit@users.noreply.github.com>
2024-04-23 14:27:50 +0200

    house

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Object = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLadder, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level3`)
 })
@@ -6,3 +9,6 @@ tiles.setCurrentTilemap(tilemap`level2`)
 let mySprite = sprites.create(assets.image`Snek`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 7))
+let House = sprites.create(assets.image`houseRed`, SpriteKind.Object)
+tiles.placeOnTile(House, tiles.getTileLocation(5, 5))
